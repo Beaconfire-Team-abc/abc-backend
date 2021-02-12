e06d4c521357a3c4843ab2d1daefe334db9057dc
xuan-liu <lxuan95@163.com>
2021-02-12 11:48:00 -0500

    init core services

---
+++ timesheet/src/test/java/com/beaconfireabc/timesheet/TimesheetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beaconfireabc.timesheet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimesheetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
